33c76183e6c4c290358a7fc6e57d3957b53d8628
Maou D Pancko <djaltair146@gmail.com>
2024-10-29 14:48:44 +0300

    Чистка

--- build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_DiplomaQt_FILE F:/Qt_projs/DiplomaQt/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/DiplomaQt.exe)
-set(__QT_DEPLOY_TARGET_DiplomaQt_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_DiplomaQt_RUNTIME_DLLS C:/Qt/6.8.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Core.dll)
